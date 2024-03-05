8f789dd9ab188b3b4daf2f618fde663db912fa5b
Sandygit1525 <162268561+Sandygit1525@users.noreply.github.com>
2024-03-05 21:52:08 +0530

    Add files via upload
    Day2

---
+++ day2/FindOddNumbers.java
@@ -0,0 +1,18 @@
+package week1.day2;
+
+import java.util.Iterator;
+
+public class FindOddNumbers {
+	public static void main(String[] args) {
+		int number=10;
+		for (int i = 1; i<=number;i++) {
+			if (i%2 != 0)
+			{
+				System.out.println(i);
+				
+			
+		}
+	}
+	}
+
+}
